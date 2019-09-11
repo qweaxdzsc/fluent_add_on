a3bfbefe04f5512182288468ec7858269c1b0502
Jin <zonghui.jin@estra-automotive.com>
2019-09-11 09:54:46 +0800

    update main gui about temp_label, also alter the dir in GUI_convertor because of ui dir change

--- fluent_gui/GUI_convertor.py
+++ fluent_gui/GUI_convertor.py
@@ -2,12 +2,15 @@ import os
 
 
 def convert(py_name, gui_name):
-    os.system(r'cd C:\Users\BZMBN4\Desktop && pyuic5 -o %s %s' %(py_name, gui_name))
+    os.system(r'cd C:\Users\BZMBN4\Desktop\fluent_add_on\ui && pyuic5 -o %s %s' %(py_name, gui_name))
 
 
 gui_name = 'easy_test.ui'
 py_name = gui_name.replace('.ui', '.py')
 
 
+
+
+
 convert(py_name, gui_name)
-os.system(r'cd C:\Users\BZMBN4\Desktop && %s' %(py_name))
+os.system(r'cd C:\Users\BZMBN4\Desktop\fluent_add_on\fluent_gui && %s' %(py_name))
